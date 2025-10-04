8d615bcc1a3c5da350d1a403ed13646d2a1057ae
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-04 12:50:39 +0300

    get json info from reque and write in file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import random as rand
 import re
 import time
@@ -21,10 +22,36 @@ def getCookiesFromResponse():
 def getMangaInfo(mangaPathName):
     url = "https://mangalib.me/ru/manga/" + mangaPathName + "?section=info"
     response = getFirstResponse(url)
+    # getCookiesFromResponse()
     info = {"available-status": False}
+
     if response.status_code == 200:
         info["available-status"] = True
-    # getCookiesFromResponse()
+    else:
+        return info
+
+    header = {
+        "Client-Time-Zone": "Europe/Moscow",
+        "Content-Type": "application/json",
+        "Referer": "https://mangalib.me/",
+        'sec-ch-ua': "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Google Chrome\";v=\"140\"",
+        'sec-ch-ua-mobile': "?0",
+        'sec-ch-ua-platform': "\"Windows\"",
+        'Site-Id': "1",  # important field
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+    }
+    querystring = {
+        "fields[]": ["background", "eng_name", "otherNames", "summary", "releaseDate", "type_id", "caution",
+                         "views", "close_view", "rate_avg", "rate", "genres", "tags", "teams", "user", "franchise",
+                         "authors", "publisher", "userRating", "moderated", "metadata", "metadata.count",
+                         "metadata.close_comments", "manga_status_id", "chap_count", "status_id", "artists",
+                         "format"]
+    }
+    url = "https://api.cdnlibs.org/api/manga/" + mangaPathName
+    response = requests.request("GET", url, headers = header, params = querystring)
+    with open("data.json", "w", encoding = 'utf-8') as f:
+        json.dump(response.json(), f, ensure_ascii = False, indent=4)
+
     pass
 
 def main():
