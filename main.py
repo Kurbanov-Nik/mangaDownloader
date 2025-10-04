14bb5e5dc9594389aec7d6372a107864ce65b192
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-04 10:50:50 +0300

    try get manga info from web page

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import requests
+import random as rand
+import re
+import time
+
+def getFirstResponse(url):
+    header = {
+        "Referer": "https://mangalib.me/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+    }
+    response = requests.request("GET", url = url, headers = header)
+    return response
+
+def getCookiesFromResponse():
+    pass
+
+def getMangaInfo(url):
+    response = getFirstResponse(url)
+    info = {"available-status": False}
+    if response.status_code == 200:
+        info["available-status"] = True
+    # getCookiesFromResponse()
+    pass
 
 def main():
-    imgUrl = ("https://img3.mixlib.me"
-              "//manga/four-knights-of-the-apocalypse/chapters/3516354/36e9c392-753f-49eb-aec6-5987d9614157.jpg")
-
-    headers = {
-        'Origin' : 'https://mangalib.me',
-        'Referer' : 'https://mangalib.me/ru/54750--four-knights-of-the-apocalypse/read/v21/c185'
-    }
-
-
-    r = requests.get(imgUrl, headers = headers)
-    print(r.content)
-    with open("01.jpg", 'wb') as f:
-        f.write(r.content)
-    # pass
+    url = "https://mangalib.me/ru/manga/141625--bunsin-eulo-jadongsanyan"
+    getMangaInfo(url)
 
 if __name__ == "__main__":
     main()
