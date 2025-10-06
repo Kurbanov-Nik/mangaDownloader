9a16252215b50ed2d2210b4c655f7289371b2f1f
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-06 21:00:31 +0300

    trying download one manga chapter

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import re
 import requests
 import os
 import json
+import time
+import random as rand
 
 
 def getUrlMangaName(url):
@@ -72,7 +74,18 @@ def getTranslateBranchesInfo(url):
     with open("%s/chaptersInfo.json" % urlMangaName, "r", encoding = "utf-8") as file:
         chaptersInfo = json.load(file)["data"]
     branchesInfo = []
+    numbersByVolumes = []
+    curVol = "1"
+    numList = []
     for chapter in chaptersInfo:
+        if curVol == chapter["volume"]:
+            numList.append(chapter["number"])
+        else:
+            curVol = chapter["volume"]
+            numbersByVolumes.append(numList)
+            numList = []
+            numList.append(chapter["number"])
+
         for branch in chapter["branches"]:
             idB = branch["branch_id"]
             teams = []
@@ -98,7 +111,8 @@ def getTranslateBranchesInfo(url):
                      "users": [tempBranch["user"]],
                      "n": [tempBranch["n"]]}
                 )
-    return branchesInfo
+    numbersByVolumes.append(numList)
+    return branchesInfo, numbersByVolumes
 
 def printBranchesInfo(branchesInfo):
     line = "Всего веток: %d\n" % len(branchesInfo)
@@ -142,8 +156,44 @@ def userBranchSelection(branchAmount):
         break
     return int(num)
 
-def dowloadChapters(branchInfo):
-    pass
+def dowloadChapters(url, branchInfo, numbersByVolumes):
+    urlMangaName = getUrlMangaName(url)
+    url = "https://api.cdnlibs.org/api/manga/%s/chapter" % urlMangaName
+    header = {
+        "Client-Time-Zone": "Europe/Moscow",
+        "Content-Type": "application/json",
+        "Referer": "https://mangalib.me/",
+        'sec-ch-ua': "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Google Chrome\";v=\"140\"",
+        'sec-ch-ua-mobile': "?0",
+        'sec-ch-ua-platform': "\"Windows\"",
+        'Site-Id': "1",  # important field
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+    }
+    queryString = {}
+    if branchInfo["id"]:
+        queryString["branch_id"] = str(branchInfo["id"])
+    for chapter in branchInfo["n"]:
+        volume = "1"
+        for i in range(0, len(numbersByVolumes)):
+            if chapter in numbersByVolumes[i]:
+                volume = str(i + 1)
+                break
+        queryString["number"] = chapter
+        queryString["volume"] = volume
+        response = requests.request("GET", url, headers = header, params = queryString)
+        chapterInfo = response.json()
+        savePath = "%s\\vol.%s\\chp.%s" % (urlMangaName, volume, chapter)
+        if not os.path.exists(savePath):
+            os.makedirs(savePath)
+        j = 1
+        for page in chapterInfo["data"]["pages"]:
+            imgUrl = "https://img3.mixlib.me" + page["url"]
+            img = requests.get(imgUrl, headers = header)
+            with open("%s/vol.%s/chp.%s/%d.jpg" % (urlMangaName, volume, chapter, j), 'wb') as file:
+                file.write(img.content)
+            j += 1
+            time.sleep(7 + int(rand.random() * 10))
+        break
 
 def main():
     # url = "https://mangalib.me/ru/manga/141625--bunsin-eulo-jadongsanyan" # Авто-охота с клонами
@@ -151,12 +201,12 @@ def main():
     url = "https://mangalib.me/ru/manga/214416--monokuro-no-futari" # Монохромная пара
     # url = "https://mangalib.me/ru/manga/57093--aisha" # Айша
     collectMangaInfo(url)
-    branchesInfo = getTranslateBranchesInfo(url)
+    branchesInfo, numbersByVolumes = getTranslateBranchesInfo(url)
     printBranchesInfo(branchesInfo)
-    branchIndx = 0
+    branchIndx = 1
     if len(branchesInfo) > 1:
         branchIndx = userBranchSelection(len(branchesInfo))
-    dowloadChapters(branchesInfo[branchIndx])
+    dowloadChapters(url, branchesInfo[branchIndx - 1], numbersByVolumes)
 
 if __name__ == "__main__":
     main()
