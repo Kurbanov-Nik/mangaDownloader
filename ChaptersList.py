2c85e922791c595cfb0d850c8d91458d32fce25e
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-13 20:22:43 +0300

    reinitialize class

--- ChaptersList.py
+++ ChaptersList.py
@@ -1,14 +1,6 @@
 class ChaptersList:
     def __init__(self):
-        cpList = []
-        # id = None
-        # volume = ""
-        # number = ""
-        # name = ""
-        # publishDate = ""
-        # publishUser = ()
-        # teams = []
-        # restrictedView = False
+        chaptersList = [] # id_in_db, volume, number, name
 
     def addChapter(self):
         pass
