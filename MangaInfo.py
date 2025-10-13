60df3c7e2b275b9c0cdc5161e3b3730c71aa00f9
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-13 20:23:26 +0300

    reinitialize class

--- MangaInfo.py
+++ MangaInfo.py
@@ -1,14 +1,21 @@
+import ChaptersList
+
 class MangaInfo:
-    def __init__(self):
-        id = None
-        name = ""
-        ageRestriction = ()
-        type = ()
-        closeView = False
-        releaseDate = ""
-        status = ()
-        scanlateStatus = ()
-        branches = []
+    def __init__(self, **kwargs):
+        self.id: int = kwargs["id"]
+        self.name: str = kwargs["name"]
+        self.ageRestriction: tuple = kwargs["ageRestriction"]
+        self.type: tuple = kwargs["type"]
+        self.closeView: bool = kwargs["closeView"]
+        self.releaseDate: str = kwargs["releaseDate"]
+        self.status: tuple = kwargs["status"]
+        self.scanlateStatus: tuple = kwargs["scanlateStatus"]
+        self.branches = []
+        self.chapters = ChaptersList()
+
+    def addChapter(self):
+        pass
 
     def __str__(self):
+        self.chapters = 1
         pass
