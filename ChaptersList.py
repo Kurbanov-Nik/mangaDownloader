fed3cdd39b04bc4f19a7da045546bf9b9977991c
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-13 20:07:55 +0300

    rename file

---
+++ ChaptersList.py
@@ -0,0 +1,17 @@
+class ChaptersList:
+    def __init__(self):
+        cpList = []
+        # id = None
+        # volume = ""
+        # number = ""
+        # name = ""
+        # publishDate = ""
+        # publishUser = ()
+        # teams = []
+        # restrictedView = False
+
+    def addChapter(self):
+        pass
+
+    def __str__(self):
+        pass
