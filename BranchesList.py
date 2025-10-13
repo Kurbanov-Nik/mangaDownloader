a5fb8ad9b86428a66d82494a84e2e0b8710389e6
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-13 20:22:13 +0300

    initialize BranchesList class

---
+++ BranchesList.py
@@ -0,0 +1,11 @@
+import BranchInfo
+
+class BranchesList:
+    def __init__(self):
+        branchesList = {} # branch_id, class BranchInfo
+
+    def addChapter(self):
+        pass
+
+    def __str__(self):
+        pass
