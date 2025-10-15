4723b61ae3206a1901057fd235998521f13c5f4f
Nikita Kurbanov <valentaybequi@gmail.com>
2025-10-15 13:25:04 +0300

    realization of method addChapter()

--- BranchInfo.py
+++ BranchInfo.py
@@ -1,11 +1,18 @@
 class BranchInfo:
     def __init__(self):
-        self.chapters = [] # id, publishDate, publishUser, teams_id, restrictedView
+        self.chapters = []
         self.teams = {}
         self.users = {}
 
-    def addChapter(self):
-        pass
-
-    def __str__(self):
-        pass
+    def addChapter(self, chapter):
+        teamsId = []
+        for team in chapter["teams"]:
+            self.teams[team[0]] = team[1]
+            teamsId.append(team[0])
+        self.users[chapter["publishUser"][0]] = chapter["publishUser"][1]
+        self.chapters.append((
+            chapter["chapterIndex"],
+            chapter["publishDate"].split("T")[0],
+            teamsId,
+            chapter["publishUser"][0],
+            chapter["restrictedView"]))
