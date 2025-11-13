cfb408c3874993025731abcbe218efd0563eff54
Nikita Kurbanov <valentaybequi@gmail.com>
2025-11-13 15:17:37 +0300

    minor changes

--- downloader_utils/UserAgentManager.py
+++ downloader_utils/UserAgentManager.py
@@ -3,21 +3,19 @@ import requests
 
 class UserAgent:
     def __init__(self):
-        self.agentList = {
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
-        }
+        self.defaultAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+        self.agentList = list()
 
     def collectFromFile(self, path = "extra_files/user-agents.txt"):
         with open(path, "r") as f:
-            data = filter(lambda x: x.strip(), f.read().splitlines())
-            self.agentList.update(data)
+            data = filter(lambda line: line.strip() and not line in self.agentList, f.read().splitlines())
+        self.agentList.extend(data)
 
     def collectFromWeb(self, url = None):
         ...
 
     def getAgent(self):
-        return rand.choice(list(self.agentList))
-
-if __name__ == "__main__":
-    obj = UserAgent()
-    obj.collectFromFile()
+        if not self.agentList:
+            return self.defaultAgent
+        rand.shuffle(self.agentList)
+        return self.agentList.pop()
